cd6b4c206c76188e3c94c2c49b9bec30b34f165e
costin.chelu <costin.chelu@johnlewis.co.uk>
2024-05-28 20:01:59 +0300

    .

--- address-service/src/main/java/ro/ccar/addressservice/AddressServiceApplication.java
+++ address-service/src/main/java/ro/ccar/addressservice/AddressServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-//@ComponentScan(basePackages = {"ro.ccar.feignsimpledemo"})
+//@ComponentScan(basePackages = {"ro.ccar.addressservice"})
 @EnableJpaRepositories
 public class AddressServiceApplication {
 
